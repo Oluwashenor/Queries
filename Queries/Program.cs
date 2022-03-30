a86bffce8c8dc07395f47131096eb029f087e9c1
Adeshina <adebayo.adeshina@venturegardengroup.com>
2022-03-30 20:41:25 +0100

    LINQ syntax and Extension Methods

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using System.Linq;
+using System;
+
+
 namespace Queries
 {
     class Program
@@ -6,7 +9,24 @@ namespace Queries
         static void Main(string[] args)
         {
             var context = new PlutoContext();
+
             //Linq Syntax
+            var query =
+                from c in context.Courses
+                where c.Name.Contains("c#")
+                orderby c.Name
+                select c;
+
+            foreach (var course in query)
+                Console.WriteLine(course.Name);
+
+            //Extension Methods
+            var courses = context.Courses
+                .Where(c => c.Name.Contains("c#"))
+                .OrderBy(c => c.Name);
+
+            foreach (var course in courses)
+                Console.WriteLine(course.Name);
         }
     }
 }
